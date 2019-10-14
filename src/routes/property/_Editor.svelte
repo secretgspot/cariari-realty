f2afc606e75494a8de1e53a7b1585bb1007a9cfb
Hooker Juggernaut <secretgspot@gmail.com>
2019-10-13 19:52:31 -0600

    editor for refactored

--- src/routes/property/_Editor.svelte
+++ src/routes/property/_Editor.svelte
@@ -131,86 +131,82 @@
 		justify-content: center;
 		align-items: center;
 		flex-direction: column;
+		background: var(--color-light);
+		padding: 3rem 0;
 		/* flex-wrap: wrap; */
 	}
-	.form-wrap {
-		display: grid;
-		grid-template-columns: repeat(1, 1fr);
-		margin: 3rem 0rem 3rem;
-		grid-row-gap: 1rem;
-		width: 90vw;
-		/* grid-template-columns: repeat(auto-fit, minmax(222px, 1fr)); */
-		/* grid-template-columns: repeat(3, 1fr); */
-		/* margin: 6rem 6rem 3rem; */
-	}
-	@media (min-width: 1024px) {
-		.form-wrap {
-			grid-template-columns: repeat(auto-fit, minmax(246px, 1fr));
-			width: 90vw;
-		}
-	}
-
-	fieldset {
-		border: 0px dotted #e6e6e6;
-		border-radius: var(--border-radius);
-	}
-	legend {
-		text-transform: uppercase;
-		font-size: 0.6em;
-		font-family: sans-serif;
-	}
-	fieldset.active { border: 1px solid var(--color-success); }
-	fieldset.removed { border: 1px solid var(--color-danger); }
 
 	fieldset input[type="text"],
 	fieldset select,
 	fieldset textarea {
 		display: block;
-		padding: 0.5rem 0.75rem;
+		padding: 0.6rem 0.81rem;
 		font-size: 1rem;
 		line-height: 1.25;
-		color: #55595c;
-		border: 1px solid rgba(0, 0, 0, 0.15);
+		color: var(--color-black);
+		border: 1px solid var(--color-white);
 		border-radius: 0.25rem;
 		width: -webkit-fill-available;
-		box-shadow: inset 0 2px 4px 0 hsla(0, 0%, 0%, 0.09);
-		background: var(--color-light);
+		box-shadow: inset 0 3px 4px 0 var(--color-dark);
 	}
-
 	fieldset :global(button) {
 		height: auto;
 		width: 100%;
 	}
 
-	fieldset.location input:first-of-type { border-bottom: 0; }
-	fieldset.location input:last-of-type {
-		border-top: 0;
-		/* border-bottom-right-radius: 0;
-		border-bottom-left-radius: 0; */
+	.section {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: auto 1fr;
+		grid-gap: 1rem;
+		padding: 1rem 2rem 2rem;
+		box-shadow: 0 1px 0px var(--color-dark), 0 2px 0px var(--color-light);
 	}
-	/* fieldset.location :global(button) {
-		border-top-right-radius: 0;
-		border-top-left-radius: 0;
-	} */
+	.section .header p { color: var(--color-darker); }
+	.section fieldset {
+		border: 0px dotted #e6e6e6;
+		border-radius: var(--border-radius);
+		margin: 0 0 2rem;
+	}
+	.section legend {
+		text-transform: uppercase;
+		font-size: 0.81em;
+		color: var(--color-dark);
+	}
 
-	.feature-list {
-		margin: 1rem 0;
+	@media (min-width: 768px) {
+		.section {
+			grid-template-columns: minmax(auto, 270px) minmax(auto, 540px);
+			grid-template-rows: 1fr;
+			grid-gap: 2rem;
+			padding: 2rem;
+		}
+		.section fieldset { margin: 0 0 2rem; }
 	}
-	.feature {
+
+	.section_property_type .active { border: 1px solid var(--color-success); }
+	.section_property_type .removed { border: 1px solid var(--color-danger); }
+	.section_property_type .checkbox { display: inline-block; }
+	.section_property_type .checkbox:nth-child(odd) { padding: 1rem; }
+
+	.section_location .location input:first-of-type { border-bottom: 0; }
+	.section_location .location input:last-of-type { border-top: 0; }
+
+	.section_features .feature-list { margin: 1rem 0; }
+	.section_features .feature {
 		display: inline-flex;
 		align-items: center;
-		/* border: 1px solid var(--color-black); */
-		background: var(--color-light);
+		background: var(--color-white);
 		color: var(--color-darker);
-		border-radius: 6px;
+		border-radius: var(--border-radius);
 		padding: 0.2rem 0.4rem 0.2rem 0.2rem;
 		margin: 0.2rem;
+		box-shadow: 0 1px 2px var(--color-dark);
 	}
-	.feature .close {
+	.section_features .feature .close {
 		width: 18px;
 		height: 18px;
 		color: var(--color-dark);
-		/* border: 1px dotted var(--color-magenta); */
 		border-radius: var(--border-radius);
 		margin-right: 0.2rem;
 		position: relative;
@@ -219,7 +215,7 @@
 		font-style: normal;
 		font-weight: bold;
 	}
-	.feature .close::after {
+	.section_features .feature .close::after {
 		content: 'x';
 		font-size: 0.6rem;
 		position: absolute;
@@ -227,10 +223,9 @@
 		left: 50%;
 		transform: translate(-50%, -50%);
 	}
-
-	.section-description,
-	.section-photos { grid-column: 1 / -1; }
-	.photo-list {
+	.section_features .description,
+	.section_features .photos { grid-column: 1 / -1; }
+	.section_features .photo-list {
 		display: grid;
 		grid-template-columns: repeat(auto-fit, minmax(auto, 120px));
 		/* grid-template-rows: repeat(auto-fit, minmax(90px, 120px)); */
@@ -238,23 +233,17 @@
 		justify-content: center;
 		margin: 1rem 0;
 	}
-	.photo {
+	.section_features .photo {
 		position: relative;
-		border: 0px solid var(--color-black);
-		box-shadow: 0 2px 3px 0 hsla(0, 0%, 0%, 0.3);
+		box-shadow: 0 1px 1px var(--color-white),
+								0 2px 3px var(--color-darker);
 		border-radius: var(--border-radius);
 		text-align: center;
-		/* overflow: hidden; */
 		display: flex;
 		background-size: cover;
 		height: 120px;
 	}
-	/* .photo .preview {
-		width: 100%;
-		height: 100%;
-		object-fit: contain;
-	} */
-	.photo .close {
+	.section_features .photo .close {
 		width: 27px;
 		height: 27px;
 		color: var(--color-danger);
@@ -269,7 +258,7 @@
 		background: hsla(0, 0%, 100%, 0.81);
 		font-style: normal;
 	}
-	.photo .close::after {
+	.section_features .photo .close::after {
 		content: 'x';
 		position: absolute;
 		top: 50%;
@@ -278,10 +267,15 @@
 	}
 
 	footer.buttons-group {
-		/* margin: 3rem 6rem 6rem; */
-		margin: 1rem 2rem 6rem;
 		display: flex;
-		align-self: stretch;
+		padding: 2rem;
+	}
+	@media (min-width: 768px) {
+		footer.buttons-group { justify-content: flex-end; }
+		footer.buttons-group :global(button) {
+			flex: 0 1 auto;
+			width: 333px;
+		}
 	}
 </style>
 
@@ -303,192 +297,255 @@
 
 <section class="edit-property">
 
-	<!-- <h1>Edit/Client view {slug} {JSON.stringify(query, null, 2)}</h1> -->
-
-	<div class="form-wrap">
-
-		<fieldset>
-			<legend>Active</legend>
-			<!-- <input type="text" placeholder="ex: true" bind:value="{property.is_active}" disabled="{!$isAdmin}"> -->
-			<Toggle bind:checked="{property.is_active}" label="{property.is_active ? 'Listed' : 'Delisted'}" disabled="{!$isAdmin}" />
-		</fieldset>
-
-		<fieldset class="{property.is_active ? 'active' : 'removed'}">
-			<legend>MSL</legend>
-			<input type="text" placeholder="ex: CR-001" bind:value="{property.msl}" disabled="{query.msl}" />
-			{#if !property.msl}
-			<Button type="button" on:click="{getMsl}">Set</Button>
-			{/if}
-		</fieldset>
-
-		<fieldset>
-			<legend>Land Use</legend>
-			<select bind:value="{property.land_use}" disabled="{query.msl}">
-				<option>Residential</option>
-				<option>Commercial</option>
-				<option>Industrial</option>
-			</select>
-		</fieldset>
-
-		<fieldset>
-			<legend>Property For</legend>
-			<label>
-				<input type="checkbox" bind:group="{property.property_for}" value="Rent">	Rent
-			</label>
-			<label>
-				<input type="checkbox" bind:group="{property.property_for}" value="Sale">	Sale
-			</label>
-			<label>
-				<input type="checkbox" bind:group="{property.property_for}" value="Investment">	Investment
-			</label>
-		</fieldset>
-
-		<fieldset>
-			<legend>Address</legend>
-			<input type="text" placeholder="ex: Avenida 52, Provincia Heredia, La Asunción, 40703" bind:value="{property.address}">
-		</fieldset>
-
-		<fieldset class="location">
-			<legend>Location (lat, lng)</legend>
-			<input type="text" placeholder="ex: 9.97542" bind:value="{property.location.lat}" disabled="{query.msl}">
-			<input type="text" placeholder="ex: -84.163443" bind:value="{property.location.lng}" disabled="{query.msl}">
-			{#if !query.msl}
-			<Button type="button" on:click="{getPosition}">Get current GPS</Button>
-			{/if}
-		</fieldset>
-
-		<fieldset>
-			<legend>Email</legend>
-			<input type="text" placeholder="ex: this@that.there" bind:value="{property.contact_email}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Phone</legend>
-			<input type="text" placeholder="ex: 1234-5678" bind:value="{property.contact_phone}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Year built</legend>
-			<input type="text" placeholder="ex: 2019" bind:value="{property.year_built}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Building Style</legend>
-			<input type="text" placeholder="ex: 2 Storey" bind:value="{property.building_style}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Lot Size ㎡</legend>
-			<input type="text" placeholder="ex: 900" bind:value="{property.lot_size}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Building Size ㎡</legend>
-			<input type="text" placeholder="ex: 810" bind:value="{property.building_size}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Parking Spaces</legend>
-			<input type="text" placeholder="ex: 9" bind:value="{property.parking_spaces}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Rooms</legend>
-			<input type="text" placeholder="ex: 6" bind:value="{property.rooms_count}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Bedrooms</legend>
-			<input type="text" placeholder="ex: 3" bind:value="{property.beds_count}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Bathrooms</legend>
-			<input type="text" placeholder="ex: 3" bind:value="{property.baths_count}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Restrooms (half-baths)</legend>
-			<input type="text" placeholder="ex: 2" bind:value="{property.half_baths_count}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Price $</legend>
-			<input type="text" placeholder="ex: 630000" bind:value="{property.price}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Rent ($/month)</legend>
-			<input type="text" placeholder="ex: 1800" bind:value="{property.rent}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Taxes ($/year)</legend>
-			<input type="text" placeholder="ex: 1500" bind:value="{property.taxes}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Fees (condo, asssociation) ($/month)</legend>
-			<input type="text" placeholder="ex: 120" bind:value="{property.fees}">
-		</fieldset>
-
-		<fieldset>
-			<legend>Features</legend>
-			<input type="text" placeholder="ex: BBQ" use:enter={addFeature}>
-			<div class="feature-list">
-				{#each property.features as feature, i}
-					<span class="feature">
-						<i class="close" on:click="{() => removeFeature(i)}" />
-						{feature}
-					</span>
-				{/each}
+	<div class="sections_wrap">
+		<!-- PROPERTY TYPE -->
+		<section class="section section_property_type">
+			<div class="header">
+				<h3>Property type</h3>
+				<p>Categories listing in appropriate section and coloring on the map. Is part of filtering.</p>
 			</div>
-		</fieldset>
 
-		<fieldset class="section-description">
-			<legend>Description</legend>
-			<textarea rows="6" placeholder="Description (max 9 sentences)" bind:value="{property.description}"></textarea>
-		</fieldset>
+			<div class="inputs">
+				{#if $isAdmin}
+				<fieldset>
+					<legend>Active</legend>
+					<!-- <input type="text" placeholder="ex: true" bind:value="{property.is_active}" disabled="{!$isAdmin}"> -->
+					<Toggle bind:checked="{property.is_active}" label="{property.is_active ? 'Listed' : 'Delisted'}" disabled="{!$isAdmin}" />
+				</fieldset>
+				{/if}
 
-		<fieldset class="section-photos">
-			<legend>Photos</legend>
-			<input type="text" placeholder="ex: //placekitten.com/g/1080/810" use:enter={addPhoto}>
-			<div class="photo-list">
-				{#each property.photos as photo, i}
-					<div class="photo" style="background-image: url({photo})">
-						<i class="close" on:click="{() => removePhoto(i)}" />
-						<!-- <img class="preview" src="{photo}" alt="{photo}"> -->
+				<fieldset class="{property.is_active ? 'active' : 'removed'}">
+					<legend>MSL</legend>
+					<input type="text" placeholder="ex: CR-001" bind:value="{property.msl}" disabled="{query.msl}" />
+					{#if !property.msl}
+					<Button type="button" on:click="{getMsl}">Set</Button>
+					{/if}
+				</fieldset>
+
+				<fieldset>
+					<legend>Land Use</legend>
+					<select bind:value="{property.land_use}" disabled="{query.msl}">
+						<option>Residential</option>
+						<option>Commercial</option>
+						<option>Industrial</option>
+					</select>
+				</fieldset>
+
+				<fieldset>
+					<legend>Property For</legend>
+					<label class="checkbox">
+						<input type="checkbox" bind:group="{property.property_for}" value="Rent">	Rent
+					</label>
+					<label class="checkbox">
+						<input type="checkbox" bind:group="{property.property_for}" value="Sale">	Sale
+					</label>
+					<label class="checkbox">
+						<input type="checkbox" bind:group="{property.property_for}" value="Investment">	Investment
+					</label>
+				</fieldset>
+			</div>
+		</section>
+
+		<!-- PROPERTY LOCATION -->
+		<section class="section section_location">
+			<div class="header">
+				<h3>Location</h3>
+				<p>GPS coordinates and physical address. Location displayed on map via GPS, accuracy is important.</p>
+			</div>
+
+			<div class="inputs">
+				<fieldset>
+					<legend>Address</legend>
+					<input type="text" placeholder="ex: Avenida 52, Provincia Heredia, La Asunción, 40703" bind:value="{property.address}">
+				</fieldset>
+
+				<fieldset class="location">
+					<legend>Location (lat, lng)</legend>
+					<input type="text" placeholder="ex: 9.97542" bind:value="{property.location.lat}" disabled="{query.msl}">
+					<input type="text" placeholder="ex: -84.163443" bind:value="{property.location.lng}" disabled="{query.msl}">
+					{#if !query.msl}
+					<Button type="button" on:click="{getPosition}">Get current GPS</Button>
+					{/if}
+				</fieldset>
+			</div>
+		</section>
+
+		<!-- PROPERTY CONTACTS -->
+		<section class="section section_contacts">
+			<div class="header">
+				<h3>Contacts</h3>
+				<p>Email and Phone number of a realtor or person selling the property, usually found on a placard in front of the property.</p>
+			</div>
+
+			<div class="inputs">
+				<fieldset>
+					<legend>Phone</legend>
+					<input type="text" placeholder="ex: 1234-5678" bind:value="{property.contact_phone}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Email</legend>
+					<input type="text" placeholder="ex: this@that.there" bind:value="{property.contact_email}">
+				</fieldset>
+			</div>
+		</section>
+
+		<!-- PROPERTY BASICS -->
+		<section class="section section_basics">
+			<div class="header">
+				<h3>Basics</h3>
+				<p>These are initial values and usually first thing user looks for when narrowing down their options.</p>
+			</div>
+
+			<div class="inputs">
+				<fieldset>
+					<legend>Year built</legend>
+					<input type="text" placeholder="ex: 2019" bind:value="{property.year_built}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Building Style</legend>
+					<input type="text" placeholder="ex: 2 Storey" bind:value="{property.building_style}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Lot Size ㎡</legend>
+					<input type="text" placeholder="ex: 900" bind:value="{property.lot_size}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Building Size ㎡</legend>
+					<input type="text" placeholder="ex: 810" bind:value="{property.building_size}">
+				</fieldset>
+			</div>
+		</section>
+
+		<!-- PROPERTY DETAILS -->
+		<section class="section section_details">
+			<div class="header">
+				<h3>Details</h3>
+				<p>Information important to the user and thus should be filled in as soon as possible. Also used in filters.</p>
+			</div>
+
+			<div class="inputs">
+				<fieldset>
+					<legend>Bedrooms</legend>
+					<input type="text" placeholder="ex: 3" bind:value="{property.beds_count}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Bathrooms</legend>
+					<input type="text" placeholder="ex: 3" bind:value="{property.baths_count}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Restrooms (half-baths)</legend>
+					<input type="text" placeholder="ex: 2" bind:value="{property.half_baths_count}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Rooms</legend>
+					<input type="text" placeholder="ex: 6" bind:value="{property.rooms_count}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Parking Spaces</legend>
+					<input type="text" placeholder="ex: 9" bind:value="{property.parking_spaces}">
+				</fieldset>
+			</div>
+		</section>
+
+		<!-- PROPERTY PRICING -->
+		<section class="section section_pricing">
+			<div class="header">
+				<h3>Pricing</h3>
+				<p>Everything regarding money goes in here and is very important since used in filters and relevent to user.</p>
+			</div>
+
+			<div class="inputs">
+				<fieldset>
+					<legend>Price $</legend>
+					<input type="text" placeholder="ex: 630000" bind:value="{property.price}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Rent ($/month)</legend>
+					<input type="text" placeholder="ex: 1800" bind:value="{property.rent}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Taxes ($/year)</legend>
+					<input type="text" placeholder="ex: 1500" bind:value="{property.taxes}">
+				</fieldset>
+
+				<fieldset>
+					<legend>Fees (condo, asssociation) ($/month)</legend>
+					<input type="text" placeholder="ex: 120" bind:value="{property.fees}">
+				</fieldset>
+			</div>
+		</section>
+
+		<!-- PROPERTY FEATURES -->
+		<section class="section section_features">
+			<div class="header">
+				<h3>Features</h3>
+				<p>Description, features, photos. Photos are important and good quality should be provided.</p>
+			</div>
+
+			<div class="inputs">
+				<fieldset>
+					<legend>Features</legend>
+					<input type="text" placeholder="ex: BBQ" use:enter={addFeature}>
+					<div class="feature-list">
+						{#each property.features as feature, i}
+							<span class="feature">
+								<i class="close" on:click="{() => removeFeature(i)}" />
+								{feature}
+							</span>
+						{/each}
 					</div>
-				{/each}
+				</fieldset>
+
+				<fieldset class="description">
+					<legend>Description</legend>
+					<textarea rows="6" placeholder="Description (max 9 sentences)" bind:value="{property.description}"></textarea>
+				</fieldset>
+
+				<fieldset class="photos">
+					<legend>Photos</legend>
+					<input type="text" placeholder="ex: //placekitten.com/g/1080/810" use:enter={addPhoto}>
+					<div class="photo-list">
+						{#each property.photos as photo, i}
+							<div class="photo" style="background-image: url({photo})">
+								<i class="close" on:click="{() => removePhoto(i)}" />
+								<!-- <img class="preview" src="{photo}" alt="{photo}"> -->
+							</div>
+						{/each}
+					</div>
+				</fieldset>
 			</div>
-		</fieldset>
+		</section>
 
-	</div>
-
-	<footer class="buttons-group">
-		{#if slug && property.is_active && !$isAdmin}
-			<Button type="button" color="danger" disabled={inProgress} on:click="{remove}">Remove</Button>
-		{/if}
-		{#if slug && $isAdmin}
-			<Button type="button" color="danger" disabled={inProgress} on:click="{remove}">Delete</Button>
-		{/if}
-
-		{#if slug && !inProgress}
-			<Button type="button" disabled={inProgress} on:click="{() => { goto(`/property/${slug}?qr=true`) }}">Print</Button>
-		{/if}
-
-		<Button type="button" color="success" disabled={inProgress} on:click="{publish}">
-			{#if slug}
-				Submit Changes
-			{:else}
-				Add Property
+		<!-- BUTTONS -->
+		<footer class="buttons-group">
+			{#if slug && property.is_active && !$isAdmin}
+				<Button type="button" color="danger" disabled={inProgress} on:click="{remove}">Remove</Button>
 			{/if}
-		</Button>
-	</footer>
+			{#if slug && $isAdmin}
+				<Button type="button" color="danger" disabled={inProgress} on:click="{remove}">Delete</Button>
+			{/if}
 
+			{#if slug && !inProgress}
+				<Button type="button" disabled={inProgress} on:click="{() => { goto(`/property/${slug}?qr=true`) }}">Print</Button>
+			{/if}
 
+			<Button type="button" color="success" disabled={inProgress} on:click="{publish}">
+				{#if slug}
+					Submit Changes
+				{:else}
+					Add Property
+				{/if}
+			</Button>
+		</footer>
+	</div>
 </section>
-
-<!-- <pre>
-{JSON.stringify(property, null, 2)}
-</pre> -->
